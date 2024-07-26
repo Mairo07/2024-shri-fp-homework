390ea0d7044674be42ab9908bc611dacbeefd1ae
Daria Anashkina <mairou@yandex.ru>
2024-07-26 15:19:10 +0300

    first part completed

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,21 @@
+import {
+  map,
+  curry,
+  allPass,
+  values,
+  equals,
+  pipe,
+  length,
+  gte,
+  filter,
+  flip,
+  keys,
+  propSatisfies,
+  not,
+  prop,
+  countBy,
+} from 'ramda';
+
 /**
  * @file Домашка по FP ч. 1
  *
@@ -13,38 +31,115 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+const maxInArr = (arr) => Math.max(...arr) 
 
-    return star === 'red' && square === 'green';
-};
+//Функции-предикаты на цвета
+const isColor = curry((color, shape) => equals(color, shape));
+const isOrange = isColor('orange');
+const isGreen = isColor('green');
+const isBlue = isColor('blue');
+const isRed = isColor('red');
+const isWhite = isColor('white');
+
+//Определенная форма конкретного цвета
+const isTriangleWhite = propSatisfies(isWhite, 'triangle');
+const isCircleWhite = propSatisfies(isWhite, 'circle');
+const isSquareWhite = propSatisfies(isWhite, 'square');
+const isStarWhite = propSatisfies(isWhite, 'star');
+const isStarRed = propSatisfies(isRed, 'star');
+const isSquareGreen = propSatisfies(isGreen, 'square');
+const isTrianglGreen = propSatisfies(isGreen, 'triangle');
+const isCircleBlue = propSatisfies(isBlue, 'circle');
+const isSquareOrange = propSatisfies(isOrange, 'square');
+
+//Форма не указанного цвета
+const isNotRedStar = pipe(isStarRed, not);
+const isNotWhiteStar = pipe(isStarWhite, not);
+const isNotWhiteSquare = pipe(isSquareWhite, not);
+
+//Подсчет колличество форм определенного цвета
+const countBlue = pipe(values, filter(isBlue), length);
+const countRed = pipe(values, filter(isRed), length);
+const countGreen = pipe(values, filter(isGreen), length);
+
+//Геттеры - цвет формы
+const getTriangleColor = prop('triangle');
+const getSquareColor = prop('square');
+
+//Предикаты проверяют соответствует ли колличество форм указанного цвета заданному
+const minTwoGreenShapes = pipe(countGreen, flip(gte)(2));
+const twoGreenShapes = pipe(countGreen, equals(2));
+const oneRedShape = pipe(countRed, equals(1));
+
+//Проверяем, что разные формы одног цвета
+const threeShapesOneColor = pipe(
+    values,
+    filter(color => color !== 'white'),
+    countBy(color => color),
+    values,
+    maxInArr,
+    flip(gte)(3)
+);
+const isTriangleAndSquareOneColor = (shapes) => equals(getSquareColor(shapes), getTriangleColor(shapes));
+
+const allOrangePredicats = pipe(
+    keys,
+    map(key => propSatisfies(isOrange, key))
+);
+
+const allGreenPredicats = pipe(
+    keys,
+    map(key => propSatisfies(isGreen, key))
+);
+
+
+// 1. Красная звезда, зеленый квадрат, все остальные белые.
+export const validateFieldN1 = allPass([
+  allPass([isTriangleWhite, isCircleWhite]),
+  isStarRed,
+  isSquareGreen,
+]);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = minTwoGreenShapes;
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (shapes) => equals(countBlue(shapes), countRed(shapes));
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+    isCircleBlue,
+    isStarRed,
+    isSquareOrange
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = threeShapesOneColor
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+    twoGreenShapes,
+    isTrianglGreen,
+    oneRedShape
+]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (shapes) => {
+    const isAllOrange = allPass(allOrangePredicats(shapes))
+    return isAllOrange(shapes)   
+};
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = allPass([
+    isNotRedStar, 
+    isNotWhiteStar
+]);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (shapes) => {
+    const isAllGreen = allPass(allGreenPredicats(shapes))
+    return isAllGreen(shapes)   
+};
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([isTriangleAndSquareOneColor, isNotWhiteSquare])
